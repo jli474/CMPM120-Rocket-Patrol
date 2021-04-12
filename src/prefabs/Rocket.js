de0fe171850377fe20bbe613b512b21a69a2ef16
Jiahui Li <jli474@ucsc.edu>
2021-04-11 17:22:11 -0700

    2nd commit

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,22 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to the existing scene
+        scene.add.existing(this);
+        this.isFiring = false;         // tranck rocket firing status
+        this.moveSpeed = 2;            // pixels per frame
+    }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+    }
+}
